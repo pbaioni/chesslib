05e540c5e81a9e0cb714d1403b331b98bd1cda0b
Paolo Baioni <baioni2002@yahoo.fr>
2021-04-14 12:20:09 +0200

    purge main method

--- src/main/java/pbaioni/chesslib/main.java
+++ src/main/java/pbaioni/chesslib/main.java
@@ -1,23 +1,14 @@
 package pbaioni.chesslib;
 
-import pbaioni.chesslib.move.Influence;
-import pbaioni.chesslib.move.InfluenceGenerator;
-import pbaioni.chesslib.move.Move;
-
 public class main {
 
 	public static void main(String[] args) {
-		System.out.println("welcome");
-		// TODO Auto-generated method stub
-        Board board = new Board();
+		
+		System.out.println("Welcome to chesslib");
+
+        //Board board = new Board();
 
 
-        board.doMove(new Move(Square.G1, Square.F3));
-        Influence influence = InfluenceGenerator.generateInfluence(board);
-        board.doMove(new Move(Square.E7, Square.E5));
-        influence = InfluenceGenerator.generateInfluence(board);
-        System.out.println(influence.toString());
-        board.toString();
 
 	}
 
